eed80f87a05f9ca2ef497e007aaa43df564da1bb
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-12 17:56:15 -0600

    feat(Sidebar): implement the shadcn Sheet component within the main component

--- FrontEnd/src/components/Sidebar.jsx
+++ FrontEnd/src/components/Sidebar.jsx
@@ -1,5 +1,27 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../components/ui/sheet.jsx";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Sheet>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default Sidebar;
