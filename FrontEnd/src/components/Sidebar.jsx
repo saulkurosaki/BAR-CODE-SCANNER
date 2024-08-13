8a9d1885f2d8d098677359cfb353785a8cebfb98
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-12 18:22:12 -0600

    feat(Sidebar): implement the shopping cart icon to the sidebar sheet trigger

--- FrontEnd/src/components/Sidebar.jsx
+++ FrontEnd/src/components/Sidebar.jsx
@@ -6,21 +6,26 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../components/ui/sheet.jsx";
+import shoppingCartIcon from "../assets/shopping-cart-icon.svg";
 
 const Sidebar = () => {
   return (
-    <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
-      </SheetContent>
-    </Sheet>
+    <section className="absolute top-12 right-12">
+      <Sheet>
+        <SheetTrigger>
+          <img src={shoppingCartIcon} alt="shopping-cart" width={45} />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
   );
 };
 
--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/assets")],
+      symbolId: "icon-[name]",
+    }),
+  ],
+});
