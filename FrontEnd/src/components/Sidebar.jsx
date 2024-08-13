f03acf1df02c4b7c9e5dc61bd8a32db1d2979c42
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-13 12:22:55 -0600

    feat(Sidebar): implement the no items in the cart management

--- FrontEnd/src/components/Sidebar.jsx
+++ FrontEnd/src/components/Sidebar.jsx
@@ -10,90 +10,90 @@ import shoppingCartIcon from "../assets/shopping-cart-icon.svg";
 import ScannedItem from "./ScannedItem.jsx";
 
 const cartItems = [
-  {
-    id: crypto.randomUUID(),
-    name: "Galletas Oreo",
-    description:
-      "Galletas de chocolate rellenas de una deliciosa crema blanca por dentro que te dejara con ganas de comer mas.",
-    price: 2.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Café con Leche",
-    description:
-      "Un delicioso café mezclado con leche fresca, perfecto para empezar el día.",
-    price: 1.49,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Tortilla de Patatas",
-    description:
-      "Una tortilla de patatas casera, hecha con amor y dedicación, para disfrutar en cualquier momento.",
-    price: 3.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Pizza Margherita",
-    description:
-      "Una pizza clásica italiana, con salsa de tomate, mozzarella fresca y albahaca, perfecta para una noche de cine.",
-    price: 12.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Hamburguesa Clásica",
-    description:
-      "Una hamburguesa clásica, con carne de ternera, lechuga, tomate, cebolla y queso, para disfrutar en un día de verano.",
-    price: 8.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Ensalada de Frutas",
-    description:
-      "Una fresca ensalada de frutas, con una variedad de colores y sabores, perfecta para una comida ligera.",
-    price: 4.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Tacos de Carnitas",
-    description:
-      "Tacos de carnitas, con carne de cerdo cocida a la perfección, salsa de limón y cebolla, para disfrutar en un día festivo.",
-    price: 9.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Sushi de Atún",
-    description:
-      "Sushi de atún fresco, con arroz japonés y wasabi, perfecto para una comida exótica.",
-    price: 15.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Tortas de Aceite",
-    description:
-      "Tortas de aceite, hechas con aceite de oliva y azúcar, perfectas para un desayuno o merienda.",
-    price: 1.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Pollo al Curry",
-    description:
-      "Pollo al curry, cocido en una deliciosa salsa de curry, con arroz basmati y verduras, para disfrutar en un día de invierno.",
-    price: 10.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Ensalada de Espinacas",
-    description:
-      "Ensalada de espinacas frescas, con aguacate, huevo duro y vinagreta de limón, perfecta para una comida saludable.",
-    price: 5.99,
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "Tostada de Aguacate",
-    description:
-      "Tostada de aguacate, con aguacate fresco, sal y limón, perfecta para un snack saludable.",
-    price: 3.49,
-  },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Galletas Oreo",
+  //   description:
+  //     "Galletas de chocolate rellenas de una deliciosa crema blanca por dentro que te dejara con ganas de comer mas.",
+  //   price: 2.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Café con Leche",
+  //   description:
+  //     "Un delicioso café mezclado con leche fresca, perfecto para empezar el día.",
+  //   price: 1.49,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Tortilla de Patatas",
+  //   description:
+  //     "Una tortilla de patatas casera, hecha con amor y dedicación, para disfrutar en cualquier momento.",
+  //   price: 3.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Pizza Margherita",
+  //   description:
+  //     "Una pizza clásica italiana, con salsa de tomate, mozzarella fresca y albahaca, perfecta para una noche de cine.",
+  //   price: 12.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Hamburguesa Clásica",
+  //   description:
+  //     "Una hamburguesa clásica, con carne de ternera, lechuga, tomate, cebolla y queso, para disfrutar en un día de verano.",
+  //   price: 8.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Ensalada de Frutas",
+  //   description:
+  //     "Una fresca ensalada de frutas, con una variedad de colores y sabores, perfecta para una comida ligera.",
+  //   price: 4.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Tacos de Carnitas",
+  //   description:
+  //     "Tacos de carnitas, con carne de cerdo cocida a la perfección, salsa de limón y cebolla, para disfrutar en un día festivo.",
+  //   price: 9.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Sushi de Atún",
+  //   description:
+  //     "Sushi de atún fresco, con arroz japonés y wasabi, perfecto para una comida exótica.",
+  //   price: 15.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Tortas de Aceite",
+  //   description:
+  //     "Tortas de aceite, hechas con aceite de oliva y azúcar, perfectas para un desayuno o merienda.",
+  //   price: 1.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Pollo al Curry",
+  //   description:
+  //     "Pollo al curry, cocido en una deliciosa salsa de curry, con arroz basmati y verduras, para disfrutar en un día de invierno.",
+  //   price: 10.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Ensalada de Espinacas",
+  //   description:
+  //     "Ensalada de espinacas frescas, con aguacate, huevo duro y vinagreta de limón, perfecta para una comida saludable.",
+  //   price: 5.99,
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "Tostada de Aguacate",
+  //   description:
+  //     "Tostada de aguacate, con aguacate fresco, sal y limón, perfecta para un snack saludable.",
+  //   price: 3.49,
+  // },
 ];
 
 const Sidebar = () => {
@@ -126,9 +126,15 @@ const Sidebar = () => {
             </SheetTitle>
             <SheetDescription>
               <div className="flex flex-col space-y-5 overflow-y-auto max-h-[85vh]">
-                {cartItems.map((item, index) => (
-                  <ScannedItem item={item} index={index} />
-                ))}
+                {cartItems.length > 0 ? (
+                  cartItems.map((item, index) => (
+                    <ScannedItem item={item} index={index} />
+                  ))
+                ) : (
+                  <p className="text-center text-lg mt-56 text-[#bbb]">
+                    You don't have anything to buy yet
+                  </p>
+                )}
               </div>
             </SheetDescription>
           </SheetHeader>
