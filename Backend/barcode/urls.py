541eb461f4b6d68cc94263578e86db9d7eee2f94
Juan Bedoya <juan@Juans-MacBook-Air.local>
2024-08-15 20:44:35 -0500

    endpointToRegister

--- Backend/barcode/urls.py
+++ Backend/barcode/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ProductFromImageView , digitCode
+from .views import ProductFromImageView , digitCode, registerProduct
 
 urlpatterns = [ # Ruta para el panel de administración de Django
     path('api/scanProduct/', ProductFromImageView.as_view()),  # Ruta para tu endpoint
     path('api/digitCode/', digitCode.as_view() ) ,#ruta para el endpoint donde se ingresa codigo a mano
+    path('api/registerproduct', registerProduct.as_view()), #ruta para registrar un nuevo producto manualmente 
 ]
